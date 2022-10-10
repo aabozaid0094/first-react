e7a0906702ed27ab590e5dccb3c4b21c55ba0f64
Ahmed Abd El-Aziz Ezzat <aaezzat@mcit.gov.eg>
2022-10-10 14:36:15 +0200

    Restyle Fyn Facts Section

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,17 +1,24 @@
+import logo from '../images/logo.svg';
 import Container from 'react-bootstrap/Container';
+
 let Content = ()=>{
     return(
-        <main id="content" className='py-4'>
-            <Container>
-                <h2>Fun facts about ReactJS</h2>
-                <ul>
-                    <li>Was first released in 2013</li>
-                    <li>Was originally created by Jordan Walke.</li>
-                    <li>Has well over 100K stars on Github.</li>
-                    <li>Is maintained by Facebook</li>
-                    <li>Powers thousands of enterprise apps, including mobile apps</li>
-                </ul>
-            </Container>
+        <main id="content">
+            <section className='fun-facts py-5'>
+                <Container>
+                    <h2>Fun facts about ReactJS</h2>
+                    <ul className='fun-facts-list'>
+                        <li>Was first released in 2013</li>
+                        <li>Was originally created by Jordan Walke.</li>
+                        <li>Has well over 100K stars on Github.</li>
+                        <li>Is maintained by Facebook</li>
+                        <li>Powers thousands of enterprise apps, including mobile apps</li>
+                    </ul>
+                </Container>
+                <div className='App-logo-wrapper'>
+                    <img src={logo} className="App-logo" alt="logo" />
+                </div>
+            </section>
         </main>
     );
 };
