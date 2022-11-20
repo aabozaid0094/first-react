d817df13bb4d7fb7b9c89f735d4de190997222ab
Ahmed Abo Zaid <aabozaid0094@gmail.com>
2022-11-20 19:21:44 +0200

    Add dark schema as state & pass it to components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import Header from './components/Header';
-import ReactHeader from './components/ReactHeader';
-import Title from './components/Title';
-import Content from './components/Content';
-import Footer from './components/Footer';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Header from "./components/Header";
+import ReactHeader from "./components/ReactHeader";
+import Title from "./components/Title";
+import Content from "./components/Content";
+import Footer from "./components/Footer";
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <Header/>
-      <ReactHeader/>
-      <Title title="Home"/>
-      <Content/>
-      <Footer/>
-    </div>
-  );
+    const [dark_schema, set_dark_schema] = React.useState(true);
+
+    const handle_schema = (event) => {
+        const dark_schema_checked = event.target.checked;
+        set_dark_schema(()=>{return dark_schema_checked?true:false;})
+    };
+
+    return (
+        <div className="App">
+            <Header dark_schema={dark_schema} handle_schema={handle_schema} />
+            <ReactHeader dark_schema={dark_schema} />
+            <Title dark_schema={dark_schema} title="Home" />
+            <Content dark_schema={dark_schema} />
+            <Footer dark_schema={dark_schema} />
+        </div>
+    );
 }
 
 export default App;
